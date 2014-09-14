08f451ac46aeaca21265e05fca37a821b014b67e
Jay Steele <steeljm1@op.ac.nz>
2014-09-14 22:39:01 +1200

    update create.pp

--- puppet/modules/mysql/manifests/create.pp
+++ puppet/modules/mysql/manifests/create.pp
@@ -3,6 +3,13 @@ class mysql::create{
     $user       = moodleuser
     $password   = M3dm00d13Db
     $name       = moodle
+    $host       = localhost
+    
+   
+    
+    
+    
+    
     
     
     }
@@ -11,18 +18,18 @@ class mysql::create{
     
     
     
-    define db( $user, $password ) {
-      exec { "create-${name}-db":
-        unless => "/usr/bin/mysql -uroot ${name}",
-        command => "/usr/bin/mysql -uroot -e \"create database ${name};\"",
-        require => Class["mysql::install"],
-        notify => Class["mysql::service"],
-      }
-
-      exec { "grant-${name}-db":
-        unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
-        command => "/usr/bin/mysql -uroot -e \"grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
-        require => [Class["mysql::service"], Exec["create-${name}-db"]]
-      }
-    
-    }
+#    define db( $user, $password ) {
+#      exec { "create-${name}-db":
+#        unless => "/usr/bin/mysql -uroot ${name}",
+#        command => "/usr/bin/mysql -uroot -e \"create database ${name};\"",
+#        require => Class["mysql::install"],
+#        notify => Class["mysql::service"],
+#      }
+#
+#      exec { "grant-${name}-db":
+#        unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
+#        command => "/usr/bin/mysql -uroot -e \"grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
+#        require => [Class["mysql::service"], Exec["create-${name}-db"]]
+#      }
+#    
+#    }
