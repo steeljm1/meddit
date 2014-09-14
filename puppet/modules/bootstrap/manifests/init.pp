914a172b7e4b123d7b9095bba1c63e217df01375
Jay Steele <steeljm1@op.ac.nz>
2014-09-14 12:15:24 +1200

    bootstrap - disable apt-get update

--- puppet/modules/bootstrap/manifests/init.pp
+++ puppet/modules/bootstrap/manifests/init.pp
@@ -12,10 +12,10 @@
 #
 class bootstrap {
   
-        # ensure local apt cache index is up to date before beginning
-        exec { 'apt-get update':
-              command => '/usr/bin/apt-get update'
-        }
+#        # ensure local apt cache index is up to date before beginning
+#        exec { 'apt-get update':
+#              command => '/usr/bin/apt-get update'
+#        }
 
 
 	# ensure local apt cache index is up to date before beginning
