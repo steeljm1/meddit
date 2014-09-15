d443b349fcdcd4b882f1c3a94d09425b7eacb6f6
Jay Steele OP <steeljm1@student.op.ac.nz>
2014-09-15 17:30:12 +1200

    working moodle user, db create, import - unhashed passwd

--- puppet/modules/mysql/manifests/create.pp
+++ puppet/modules/mysql/manifests/create.pp
@@ -1,35 +1,54 @@
 class mysql::create{ 
     
-    $user       = moodleuser
-    $password   = M3dm00d13Db
-    $name       = moodle
-    $host       = localhost
-    
-   
-    
-    
-    
-    
-    
-    
-    }
-    
-    
-    
-    
-    
-#    define db( $user, $password ) {
-#      exec { "create-${name}-db":
-#        unless => "/usr/bin/mysql -uroot ${name}",
-#        command => "/usr/bin/mysql -uroot -e \"create database ${name};\"",
-#        require => Class["mysql::install"],
-#        notify => Class["mysql::service"],
-#      }
+	       ####  Create databases   #####
+
+        # Global variables
+        $db_host      = 'localhost'
+
+        # root password
+        #$root_password  =  '*2B5396BABAD4AC79025680BDC14F8669DD1414BD'
+        #$root_password  =  '*FC0AB3C571070AFC1E7F420C4DECB92A249682F0'
+
+        $root_password  = 'M3dL@mP'
+
+        # Moodle db
+        #$moodle_password  = '*9C167602ADE8001A2D66B729D3A2E035FD2BA75B'
+        $moodle_password  = 'M3dm00d13Db'
+
+        $moodledb = 'moodle'
+        $user            = 'moodleuser'
+
+#        exec {"create_moodle_user":
 #
-#      exec { "grant-${name}-db":
-#        unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
-#        command => "/usr/bin/mysql -uroot -e \"grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
-#        require => [Class["mysql::service"], Exec["create-${name}-db"]]
-#      }
-#    
-#    }
+#             # command => "/usr/bin/mysql -uroot -p$root_password -e \"CREATE DATABASE ${moodledb}; GRANT ALL ON ${moodledb}.* TO '${user}'@'localhost' IDENTIFIED BY '${moodle_password}';\"",
+#             command => "/usr/bin/mysql -uroot -p$root_password -e \"CREATE DATABASE ${moodledb}; CREATE USER '${user}'@'localhost' IDENTIFIED BY '${moodle_password}'; GRANT ALL ON ${moodledb}.* TO '${user}'@'localhos$
+#        }
+
+        # Push database
+        file { "/root/moodle-database.sql":
+              ensure    => present,
+              source    => "puppet:///modules/mysql/moodle-database.sql",
+        }
+
+        exec {'importMoodleDb':
+
+             command => "/usr/bin/mysql -uroot -p$root_password ${moodledb} < /root/moodle-database.sql",
+             require => File["/root/moodle-database.sql"],
+
+       }
+
+
+
+        # Create Silverstripe db
+
+
+
+
+
+        # Mediawiki
+
+        
+        
+        
+	
+  }
